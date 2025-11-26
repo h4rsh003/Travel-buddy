cc0de25fe49eb1f855e6b4bfdcd7b6e4a54c4c52
TC-harshshrivastava <harsh.shrivastava@techcarrel.com>
2025-11-26 18:58:10 +0530

    Added User entity with optional password

---
+++ backend/src/entities/User.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ nullable: true }) 
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
