ab248b6b4ca58c477bf90bee08778fd0377e5622
TC-harshshrivastava <harsh.shrivastava@techcarrel.com>
2026-05-15 17:28:21 +0530

    remove localhost

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,10 +16,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: FRONTEND_URL,
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true
   }
